740425feeaa561191fc273d7d1deaa4ea156d36d
eymikhievich <eymikhievich@sberbank.ru>
2024-05-23 17:16:40 +0300

    init

---
+++ main.go
@@ -0,0 +1,51 @@
+package mymathtest
+
+import "math"
+
+func Sqrt(x float64) float64 {
+	return math.Sqrt(x)
+}
+
+func Pow(x, y float64) float64 {
+	return math.Pow(x, y)
+}
+
+func Floor(x float64) float64 {
+	return math.Floor(x)
+}
+
+func Abs(x float64) float64 {
+	return math.Abs(x)
+}
+
+func Exp(x float64) float64 {
+	return math.Exp(x)
+}
+
+func Max(x, y float64) float64 {
+	return math.Max(x, y)
+}
+
+func Yn(n int, x float64) float64 {
+	return math.Yn(n, x)
+}
+
+func Cos(x float64) float64 {
+	return math.Cos(x)
+}
+
+func Acos(x float64) float64 {
+	return math.Acos(x)
+}
+
+func Acosh(x float64) float64 {
+	return math.Acosh(x)
+}
+
+func Asin(x float64) float64 {
+	return math.Asin(x)
+}
+
+func Atan(x float64) float64 {
+	return math.Atan(x)
+}
